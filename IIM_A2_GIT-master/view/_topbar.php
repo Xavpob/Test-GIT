bff6455b2ac771a84737500ef33fe4ddb9477782
vincent ballerin <vincent ballerin>
2016-09-12 18:14:46 +0200

    Changes translate english logout

--- IIM_A2_GIT-master/view/_topbar.php
+++ IIM_A2_GIT-master/view/_topbar.php
@@ -7,7 +7,7 @@
 						<a href="index.php" class="logo"><i class="fa fa-soundcloud"></i></a>
 					</div>
 					<div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
-						<a href="logout.php" class="btn">Déconnexion</a>
+						<a href="logout.php" class="btn">Logout</a>
 						<a href="edit_image.php" class="btn">Profil</a>
 						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Ajouter une musique</a>
 					</div>
