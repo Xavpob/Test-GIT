a1cbce76d6ac39c412516a4dd4450a2d85570cfd
vincent ballerin <vincent ballerin>
2016-09-14 11:19:38 +0200

    Translate edit

--- IIM_A2_GIT-master/view/edit.php
+++ IIM_A2_GIT-master/view/edit.php
@@ -4,17 +4,17 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<div id="musicfeed">
-					<h1><i class="fa fa-pencil"></i> Editer la musique</h1>
+					<h1><i class="fa fa-pencil"></i> Edit music</h1>
 					<div class="block animated fadeInDown">
 						<div class="row">
 							<div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
 								<b class="title"><?php echo $music['title']; ?></b>
 								<form action="#" method="POST">
 									<div class="form-group">
-										<label for="title">Titre</label>
+										<label for="title">Title</label>
 										<input type="text" name="title" class="form-control" value="<?php echo $music['title']; ?>"/>
 									</div>
-									<p class="clearfix"><button type="submit" class="valid pull-right"><i class="fa fa-check"></i> Valider</button></p>
+									<p class="clearfix"><button type="submit" class="valid pull-right"><i class="fa fa-check"></i> Validate</button></p>
 								</form>
 							</div>
 						</div>
