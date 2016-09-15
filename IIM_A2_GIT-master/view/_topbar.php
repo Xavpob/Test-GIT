23010d70fb218de62672e7e2f4fdbcf147390553
Xaverine POBELLE <xaverinepobelle@orange.fr>
2016-09-15 19:25:25 +0200

    change logo

--- IIM_A2_GIT-master/view/_topbar.php
+++ IIM_A2_GIT-master/view/_topbar.php
@@ -4,12 +4,12 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-2 col-sm-2 col-md-1 col-lg-1 text-center">
-						<a href="index.php" class="logo"><i class="fa fa-soundcloud"></i></a>
+						<a href="index.php" class="logo"><img src="images/logo_new.png" style="width:50%;"></a>
 					</div>
 					<div class="col-xs-10 col-sm-10 col-md-11 col-lg-11">
 						<a href="logout.php" class="btn">Logout</a>
 						<a href="edit_image.php" class="btn">Profil</a>
-						<a href=".-/add_music.php" class="btn"><i class="fa fa-plus"></i> Add a music</a>
+						<a href="add_music.php" class="btn"><i class="fa fa-plus"></i> Add a music</a>
 						<a href="dashboard.php" class="btn">Home</a>
 					</div>
 				</div>
