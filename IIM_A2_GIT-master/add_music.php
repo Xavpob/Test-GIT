212c60564c412c0759f62cecdddab1360d1892d4
Vincent.B <vincent.ballerin@hotmail.fr>
2016-09-15 17:26:37 +0200

    V1 add_music (non fonctionnel)

--- IIM_A2_GIT-master/add_music.php
+++ IIM_A2_GIT-master/add_music.php
@@ -3,10 +3,10 @@ session_start();
 require('config/config.php');
 require('model/functions.fn.php');
 
-if( isset($_FILES['music']) && !empty($_FILES['music']) && 
+if( isset($_FILES['file']) && !empty($_FILES['file']) &&
 	isset($_POST['title']) && !empty($_POST['title'])){
 	
-	$file = $_FILES['music'];
+	$file = $_FILES['file'];
 
 	// Si le "fichier" reçu est bien un fichier
 		$ext = strtolower(substr(strrchr($file['name'], '.')  ,1));
@@ -15,14 +15,22 @@ if( isset($_FILES['music']) && !empty($_FILES['music']) &&
 			$filename = md5(uniqid(rand(), true));
 			$destination = "musics/{$filename}.{$_SESSION['id']}.{$ext}";
 
-			// TODO
+			$title = htmlspecialchars($_POST["title"]);
+
+			$request = $db->prepare("
+                INSERT INTO musics (user_id, title, file, created_at) 
+                VALUES (:user_id, :title, :file, NOW())"
+			);
+			$request->execute( array("title" => $title, "file" => $file, "user_id" => $_SESSION["user_id"]));
+			header ("Location:dashboard.php");
+		}
 
 		} else {
 			$error = 'Error, the file is not an authorized extension!';
 		}
-	// }
-}
+
 
 include 'view/_header.php';
 include 'view/add_music.php';
 include 'view/_footer.php';
+?>
