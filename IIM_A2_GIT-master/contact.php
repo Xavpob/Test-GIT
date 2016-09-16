6c0a048b196042e7700d3aeb13eb6668bfe19971
Unknown <guilllaumeaxel@gmail.com>
2016-09-16 14:30:19 +0200

    Add contact page

---
+++ IIM_A2_GIT-master/contact.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require('config/config.php');
+require('model/functions.fn.php');
+
+/*===============================
+	Contact
+===============================*/
+
+include 'view/_header.php';
+include 'view/contact.php';
+include 'view/_footer.php';
---
+++ IIM_A2_GIT-master/view/contact.php
@@ -0,0 +1,12 @@
+<body>
+	<?php include '_topbar.php'; ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			
+			</div>
+		</div>
+	</div>
+</body>
+
+
